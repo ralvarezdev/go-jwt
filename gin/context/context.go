0d9029962ed92a21efaba3d8a2a4b0cb2fab3604
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-01-11 21:51:22 -0400

    fix: solved missing token claims in context bug on net/http implementations
    * Solved missing token claims in context bug on net/http implementations
    * Removed raw token context setters and getters

--- gin/context/context.go
+++ gin/context/context.go
@@ -4,34 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	gojwt "github.com/ralvarezdev/go-jwt"
-	gojwtgin "github.com/ralvarezdev/go-jwt/gin"
 )
-
-// SetCtxRawToken sets the raw token in the context
-func SetCtxRawToken(ctx *gin.Context, rawToken *string) {
-	ctx.Set(gojwtgin.AuthorizationHeaderKey, *rawToken)
-}
 
 // SetCtxTokenClaims sets the token claims in the context
 func SetCtxTokenClaims(ctx *gin.Context, claims *jwt.MapClaims) {
 	ctx.Set(gojwt.CtxTokenClaimsKey, *claims)
-}
-
-// GetCtxRawToken tries to get the raw token from the context
-func GetCtxRawToken(ctx *gin.Context) (string, error) {
-	// Get the token from the context
-	value := ctx.Value(gojwtgin.AuthorizationHeaderKey)
-	if value == nil {
-		return "", gojwt.ErrMissingTokenInContext
-	}
-
-	// Check the type of the value
-	rawToken, ok := value.(string)
-	if !ok {
-		return "", gojwt.ErrUnexpectedTokenTypeInContext
-	}
-
-	return rawToken, nil
 }
 
 // GetCtxTokenClaims tries to get the token claims from the context
--- net/http/context/context.go
+++ net/http/context/context.go
@@ -4,42 +4,13 @@ import (
 	"context"
 	"github.com/golang-jwt/jwt/v5"
 	gojwt "github.com/ralvarezdev/go-jwt"
-	gojwtgin "github.com/ralvarezdev/go-jwt/gin"
-	gojwthttp "github.com/ralvarezdev/go-jwt/net/http"
 	"net/http"
 )
 
-// SetCtxRawToken sets the raw token in the context
-func SetCtxRawToken(r *http.Request, rawToken *string) {
-	ctx := context.WithValue(
-		r.Context(),
-		gojwthttp.AuthorizationHeaderKey,
-		*rawToken,
-	)
-	r = r.WithContext(ctx)
-}
-
 // SetCtxTokenClaims sets the token claims in the context
-func SetCtxTokenClaims(r *http.Request, claims *jwt.MapClaims) {
+func SetCtxTokenClaims(r *http.Request, claims *jwt.MapClaims) *http.Request {
 	ctx := context.WithValue(r.Context(), gojwt.CtxTokenClaimsKey, *claims)
-	r = r.WithContext(ctx)
-}
-
-// GetCtxRawToken tries to get the raw token from the context
-func GetCtxRawToken(r *http.Request) (string, error) {
-	// Get the token from the context
-	value := r.Context().Value(gojwtgin.AuthorizationHeaderKey)
-	if value == nil {
-		return "", gojwt.ErrMissingTokenInContext
-	}
-
-	// Check the type of the value
-	rawToken, ok := value.(string)
-	if !ok {
-		return "", gojwt.ErrUnexpectedTokenTypeInContext
-	}
-
-	return rawToken, nil
+	return r.WithContext(ctx)
 }
 
 // GetCtxTokenClaims tries to get the token claims from the context
