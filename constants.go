185550c298cb3976fff8a2058c629636ef912a4b
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-11-03 10:50:06 -0400

    refactor: added internal type context keys

--- constants.go
+++ constants.go
@@ -1,5 +1,10 @@
 package gojwt
 
+type (
+	// CtxKey is the type for the context keys
+	CtxKey string
+)
+
 const (
 	// BearerPrefix is the prefix for the bearer token
 	BearerPrefix = "Bearer"
@@ -7,10 +12,10 @@ const (
 
 var (
 	// CtxTokenClaimsKey is the key for the token claims to be set to the context
-	CtxTokenClaimsKey = "token_claims"
+	CtxTokenClaimsKey CtxKey = "token_claims"
 
 	// CtxTokenKey is the key for the token to be set to the context
-	CtxTokenKey = "token"
+	CtxTokenKey CtxKey = "token"
 
 	// IDClaim is the claim for the JWT ID
 	IDClaim = "jti"
